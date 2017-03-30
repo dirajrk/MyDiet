14023fca2aa51df9c4e4895f6cc8f019d2ab2c9a
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2017-03-30 15:41:33 +0800

    Now randomises whenever app is relaunched instead of relying on a button.

--- app/src/main/java/rk/diraj/mydiet/MainActivity.java
+++ app/src/main/java/rk/diraj/mydiet/MainActivity.java
@@ -5,8 +5,6 @@ package rk.diraj.mydiet;
 import android.support.v7.app.AppCompatActivity;
 import java.util.Random;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
         //text box
         final TextView quoteLabel = (TextView) findViewById(R.id.quoteLabel);
-        //button
-        Button pressme = (Button) findViewById(R.id.pressme);
 
         //code behind how to randomise quotes
         final String[] quotes = {"This is a salad!", "Eat healthy!", "Exercise all day!", "Be persistent"};
         int rando = (int) ((Math.random() * 10) + 1);
+        int random = (int) (Math.random() * 4);
+        quoteLabel.setText(quotes[random]);
 
         //code behind how to randomise image
         int[] photos = {R.drawable.healthy_food_0, R.drawable.healthy_food_1,R.drawable.healthy_food_2};
@@ -33,24 +31,6 @@ public class MainActivity extends AppCompatActivity {
         Random ran=new Random();
         int i=ran.nextInt(photos.length);
         image.setImageResource(photos[i]);
-
-        //button function
-        pressme.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //randomises quotes
-                int rando = (int) (Math.random() * 4);
-                quoteLabel.setText(quotes[rando]);
-                Random ran=new Random();
-
-                //randomises pics
-                int[] photos = {R.drawable.healthy_food_0, R.drawable.healthy_food_1,R.drawable.healthy_food_2};
-                ImageView image = (ImageView) findViewById(R.id.imageview1);
-                int k = ran.nextInt(photos.length);
-                image.setImageResource(photos[k]);
-            }
-        });
 
     }
 }
